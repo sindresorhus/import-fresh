484ca1719caf52b3d708fa58c734bf4fcb5a562d
Adam Mockor <adam.mockor@gmail.com>
2016-11-03 09:11:49 +0100

    fix codestyle based on .editorconfig

--- test-memory.js
+++ test-memory.js
@@ -2,13 +2,13 @@ const heapdump = require('heapdump');
 const uncached = require('./index.js');
 
 for (let i = 0; i < 100000; i++) {
-  require('./fixture.js')();
+	require('./fixture.js')();
 }
 
 heapdump.writeSnapshot('./require-' + Date.now() + '.heapsnapshot');
 
 for (let i = 0; i < 100000; i++) {
-  uncached('./fixture.js')();
+	uncached('./fixture.js')();
 }
 
 heapdump.writeSnapshot('./require-uncached-' + Date.now() + '.heapsnapshot');
