2e393eb00e1b75dd1d7e86bf40eb0f6e5fbccc33
Sindre Sorhus <sindresorhus@gmail.com>
2016-11-04 11:42:09 +0700

    cleanup #3

---
+++ heapdump.js
@@ -0,0 +1,15 @@
+'use strict';
+const heapdump = require('heapdump');
+const uncached = require('./');
+
+for (let i = 0; i < 100000; i++) {
+	require('./fixture.js')();
+}
+
+heapdump.writeSnapshot(`require-${Date.now()}.heapsnapshot`);
+
+for (let i = 0; i < 100000; i++) {
+	uncached('./fixture.js')();
+}
+
+heapdump.writeSnapshot(`require-uncached-${Date.now()}.heapsnapshot`);
--- index.js
+++ index.js
@@ -9,19 +9,21 @@ module.exports = function (moduleId) {
 	}
 
 	var filePath = resolveFrom(path.dirname(callerPath()), moduleId);
-	
+
 	// delete itself from module parent
 	if (require.cache[filePath] && require.cache[filePath].parent) {
 		var i = require.cache[filePath].parent.children.length;
+
 		while (i--) {
 			if (require.cache[filePath].parent.children[i].id === filePath) {
 				require.cache[filePath].parent.children.splice(i, 1);
 			}
 		}
 	}
+
 	// delete module from cache
 	delete require.cache[filePath];
-	
+
 	// return fresh module
 	return require(filePath);
 };
