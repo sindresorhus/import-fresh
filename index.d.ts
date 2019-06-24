bda1ffb02f2c28f713cbcd366c30f9ae0c93bc2d
Ronen Amiel <ronen.amiel@gmail.com>
2019-06-24 19:09:13 +0300

    document types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,29 @@
-declare const importFresh: {
-	(moduleId: string): any;
-};
+/**
+Import a module while bypassing the cache.
+
+@example
+```
+// foo.js
+let i = 0;
+module.exports = () => ++i;
+```
+
+```
+const importFresh = require('import-fresh');
+
+require('./foo')();
+//=> 1
+
+require('./foo')();
+//=> 2
+
+importFresh('./foo')();
+//=> 1
+
+importFresh('./foo')();
+//=> 1
+```
+*/
+declare function importFresh(moduleId: string): any;
 
 export = importFresh;
