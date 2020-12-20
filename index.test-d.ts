66079d9005bb154623ea62d29ca9ae9c623be949
Carl Fürstenberg <azatoth@gmail.com>
2020-12-20 14:29:45 +0100

    Generalize TypeScript declaration (#20)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,12 @@
 import {expectType, expectError} from 'tsd';
 import importFresh = require('.');
+import path = require('path');
 
 expectType<unknown>(importFresh('hello'));
 expectError(importFresh());
+
+expectType<(moduleId: string) => {}>(importFresh<(moduleId: string) => {}>('hello'));
+expectType<path.PlatformPath>(importFresh<path.PlatformPath>('path'));
+
+const foo = importFresh<path.PlatformPath>('path');
+expectType<path.PlatformPath>(foo);
