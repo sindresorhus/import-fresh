e95219a0db099deab2ecbce5d7135dca39215179
Sindre Sorhus <sindresorhus@gmail.com>
2022-02-24 23:54:01 +0700

    Borrow logic from `clear-module` package

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const path = require('path');
 const resolveFrom = require('resolve-from');
 const parentModule = require('parent-module');
 
+const clear = filePath => {
+	// Delete itself from module parent
+	if (require.cache[filePath] && require.cache[filePath].parent) {
+		let i = require.cache[filePath].parent.children.length;
+
+		while (i--) {
+			if (require.cache[filePath].parent.children[i].id === filePath) {
+				require.cache[filePath].parent.children.splice(i, 1);
+			}
+		}
+	}
+
+	// Remove all descendants from cache as well
+	if (require.cache[filePath] && require.cache[filePath].children) {
+		const children = require.cache[filePath].children.map(child => child.id);
+
+		// Delete module from cache
+		delete require.cache[filePath];
+
+		for (const id of children) {
+			clear(id);
+		}
+	}
+};
+
 module.exports = moduleId => {
 	if (typeof moduleId !== 'string') {
 		throw new TypeError('Expected a string');
@@ -13,19 +38,7 @@ module.exports = moduleId => {
 	const cwd = parentPath ? path.dirname(parentPath) : __dirname;
 	const filePath = resolveFrom(cwd, moduleId);
 
-	const oldModule = require.cache[filePath];
-	// Delete itself from module parent
-	if (oldModule && oldModule.parent) {
-		let i = oldModule.parent.children.length;
-
-		while (i--) {
-			if (oldModule.parent.children[i].id === filePath) {
-				oldModule.parent.children.splice(i, 1);
-			}
-		}
-	}
-
-	delete require.cache[filePath]; // Delete module from cache
+	clear(filePath);
 
 	const parent = require.cache[parentPath]; // If `filePath` and `parentPath` are the same, cache will already be deleted so we won't get a memory leak in next step
 
